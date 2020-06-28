67fd5642bcdc29f7921dfd5137d51bc6daa8d4bd
metalnodeug <metalnodeug@gmail.com>
2020-06-28 20:48:27 +0200

    move on

---
+++ GHFollowers/GHFAlertVC.swift
@@ -0,0 +1,17 @@
+//
+//  GHFAlertVC.swift
+//  GHFollowers
+//
+//  Created by metalnodeug on 28/06/2020.
+//  Copyright © 2020 metalnodeug. All rights reserved.
+//
+
+import UIKit
+
+class GHFAlertVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- GHFollowers/GHFBodyLabel.swift
+++ GHFollowers/GHFBodyLabel.swift
@@ -10,27 +10,27 @@ import UIKit
 
 class GHFBodyLabel: UILabel {
 
-  override init(frame: CGRect) {
-      super.init(frame: frame)
-      configure()
-  }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
 
-  required init?(coder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-  }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-  init(textAlignment: NSTextAlignment) {
-      super.init(frame: .zero)
-      self.textAlignment = textAlignment
-      configure()
-  }
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
 
-  private func configure() {
-      textColor = .secondaryLabel
-      adjustsFontSizeToFitWidth = true
-      minimumScaleFactor = 0.75
-      lineBreakMode = .byWordWrapping
-      translatesAutoresizingMaskIntoConstraints = false
-  }
-
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
