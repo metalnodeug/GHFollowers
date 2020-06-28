efb6aa40c48048a23f29719a8f54f2833420c64b
metalnodeug <metalnodeug@gmail.com>
2020-06-28 20:41:56 +0200

    create custom TitleLabel

---
+++ GHFollowers/GHFTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GHFTitleLabel.swift
+//  GHFollowers
+//
+//  Created by metalnodeug on 28/06/2020.
+//  Copyright © 2020 metalnodeug. All rights reserved.
+//
+
+import UIKit
+
+class GHFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
