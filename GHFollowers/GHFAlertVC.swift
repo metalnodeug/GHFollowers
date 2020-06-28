0d930522bba44176f35abeef15e54b2d4f5e9b16
metalnodeug <metalnodeug@gmail.com>
2020-06-28 21:16:23 +0200

    Configure custom alert

--- GHFollowers/GHFAlertVC.swift
+++ GHFollowers/GHFAlertVC.swift
@@ -10,8 +10,51 @@ import UIKit
 
 class GHFAlertVC: UIViewController {
 
+    let containerView  = UIView()
+    let titleLabel = GHFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GHFBodyLabel(textAlignment: .center)
+    let actionButton = GHFButton(bgColor: .systemPink, title: "OK")
+
+    var alertTitle: String?
+    var message: String?
+    var buttonLabel: String?
+
+    init(title: String, message: String, buttonLabel: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonLabel = buttonLabel
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        configureContainerView()
+    }
+
+    private func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+
+    private func configureTitleLabel() {
+        containerView.addSubview(titleLabel)
+        titleLabel.text = alertTitle ?? "Something went wrong"
     }
 
 }
