11f0bb18c56fa5f6f34c6b53ef7fbabc93613e52
metalnodeug <metalnodeug@gmail.com>
2020-06-28 20:45:37 +0200

    create custom body label

---
+++ GHFollowers/GHFBodyLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GHFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by metalnodeug on 28/06/2020.
+//  Copyright © 2020 metalnodeug. All rights reserved.
+//
+
+import UIKit
+
+class GHFBodyLabel: UILabel {
+
+  override init(frame: CGRect) {
+      super.init(frame: frame)
+      configure()
+  }
+
+  required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
+
+  init(textAlignment: NSTextAlignment) {
+      super.init(frame: .zero)
+      self.textAlignment = textAlignment
+      configure()
+  }
+
+  private func configure() {
+      textColor = .secondaryLabel
+      adjustsFontSizeToFitWidth = true
+      minimumScaleFactor = 0.75
+      lineBreakMode = .byWordWrapping
+      translatesAutoresizingMaskIntoConstraints = false
+  }
+
+}
