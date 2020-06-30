2d4e54bd30d788dc8e4572cca97a6cdf48d3cc7c
metalnodeug <metalnodeug@gmail.com>
2020-06-30 20:50:14 +0200

    CollectionView initial setup and refactor code

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -11,26 +11,42 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     var username: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            switch result {
-            case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                print(followers)
-            case .failure(let error):
-                self.presentGHFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
-            }
-        }
+        configureCollectionView()
+        configureViewController()
+        getFollower()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+
+    private func getFollower() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+                switch result {
+                case .success(let followers):
+                    print("Followers.count = \(followers.count)")
+                    print(followers)
+                case .failure(let error):
+                    self.presentGHFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+                }
+            }
     }
 
 }
