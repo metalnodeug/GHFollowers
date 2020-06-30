2683245bbf350e082df534f4ca8fd737b5939d15
metalnodeug <metalnodeug@gmail.com>
2020-06-30 21:41:54 +0200

    creating Diffable Data Source

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class FollowerListVC: UIViewController {
 
+    enum Section {
+        case main
+    }
+
     var username: String!
+    var followers: [Follower] = []
+
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
         configureViewController()
         getFollower()
+        configureDataSource()
+        updateData()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -33,20 +42,20 @@ class FollowerListVC: UIViewController {
     private func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemPink
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
 
     private func getFollower() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-                switch result {
-                case .success(let followers):
-                    print("Followers.count = \(followers.count)")
-                    print(followers)
-                case .failure(let error):
-                    self.presentGHFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
-                }
+            switch result {
+            case .success(let followers):
+                self.followers = followers
+                self.updateData()
+            case .failure(let error):
+                self.presentGHFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
+        }
     }
 
     private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
@@ -62,6 +71,23 @@ class FollowerListVC: UIViewController {
         flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + labelHeight)
 
         return flowLayout
-     }
+    }
+
+    private func configureDataSource(){
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+
+    private func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
 
 }
