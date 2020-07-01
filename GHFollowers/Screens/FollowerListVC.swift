f9ea97a251f17c5d3394b13bcc7c12a3ee2227d2
metalnodeug <metalnodeug@gmail.com>
2020-07-01 15:14:41 +0200

    create loading view for collectionView

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -50,8 +50,11 @@ class FollowerListVC: UIViewController {
     }
 
     private func getFollower(username: String, page: Int) {
+        self.showLoadingView()
+
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch result {
             case .success(let followers):
