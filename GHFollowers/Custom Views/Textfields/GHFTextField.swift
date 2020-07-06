0154a93677625f24a89eeb7c2dc2a824b0168f35
metalnodeug <metalnodeug@gmail.com>
2020-07-06 13:32:29 +0200

    refactor code

--- GHFollowers/Custom Views/Textfields/GHFTextField.swift
+++ GHFollowers/Custom Views/Textfields/GHFTextField.swift
@@ -9,55 +9,58 @@
 import UIKit
 
 class GHFTextField: UITextField {
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     private func configure() {
         translatesAutoresizingMaskIntoConstraints = false
-        layer.cornerRadius = 10
-        layer.borderWidth = 2
-        layer.borderColor = UIColor.systemGray4.cgColor
-        textColor = .label
-        tintColor = .label
-        textAlignment = .center
-        font = UIFont.preferredFont(forTextStyle: .title2)
-        adjustsFontSizeToFitWidth = true
-        minimumFontSize = 12
-        backgroundColor = .tertiarySystemBackground
-        autocorrectionType = .no
-        clearButtonMode = .whileEditing
-        placeholder = "Enter a username"
-        returnKeyType = .go
+        
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
+        
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
+        
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        returnKeyType               = .go
+        clearButtonMode             = .whileEditing
+        placeholder                 = "Enter a username"
     }
-
+    
     func showEmptyInputAnimation() {
         let shake = CABasicAnimation(keyPath: "position")
         shake.duration = 0.07
         shake.repeatCount = 2
         shake.autoreverses = true
-
+        
         let fromPoint = CGPoint(x: center.x - 10, y: center.y)
         let fromValue = NSValue(cgPoint: fromPoint)
-
+        
         let toPoint = CGPoint(x: center.x + 10, y: center.y)
         let toValue = NSValue(cgPoint: toPoint)
-
+        
         shake.fromValue = fromValue
         shake.toValue = toValue
-
+        
         let borderColor = CABasicAnimation(keyPath: "borderColor")
         borderColor.duration = 0.4
         borderColor.autoreverses = true
         borderColor.fromValue = layer.borderColor
         borderColor.toValue = UIColor.systemRed.cgColor
-
+        
         layer.add(borderColor, forKey: nil)
         layer.add(shake, forKey: nil)
     }
