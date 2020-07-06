146279c4ec9457c8b95fa7a9a1377667a86477e6
metalnodeug <metalnodeug@gmail.com>
2020-07-06 13:45:25 +0200

    move on

---
+++ GHFollowers/Custom Views/Views/GHFAlertContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GHFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by metalnodeug on 06/07/2020.
+//  Copyright © 2020 metalnodeug. All rights reserved.
+//
+
+import UIKit
+
+class GHFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
